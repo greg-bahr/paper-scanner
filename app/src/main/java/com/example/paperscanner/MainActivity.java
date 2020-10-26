67e6fda159107a19b3de805eff75587fd3302e0a
greg-bahr <gregsbahr@gmail.com>
2020-10-25 23:55:39 -0500

    camera preview and image capture

--- app/src/main/java/com/example/paperscanner/MainActivity.java
+++ app/src/main/java/com/example/paperscanner/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.paperscanner;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.paperscanner.camera.PaperScanningFragment;
 import com.example.paperscanner.scan_history.ScanHistoryFragment;
 
-public class MainActivity extends AppCompatActivity implements ScanHistoryFragment.OnCameraFabClickListener {
+public class MainActivity extends AppCompatActivity implements ScanHistoryFragment.OnCameraFabClickListener, ActivityCompat.OnRequestPermissionsResultCallback {
+    private final int PERMISSION_REQUEST_CODE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +28,11 @@ public class MainActivity extends AppCompatActivity implements ScanHistoryFragme
         ScanHistoryFragment scanHistoryFragment = new ScanHistoryFragment();
         ft.add(R.id.fragment_container, scanHistoryFragment);
         ft.commit();
+
+        boolean permissionGranted = ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+        if (!permissionGranted) {
+            ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.CAMERA }, PERMISSION_REQUEST_CODE);
+        }
     }
 
 
@@ -30,5 +43,19 @@ public class MainActivity extends AppCompatActivity implements ScanHistoryFragme
         ft.replace(R.id.fragment_container, new PaperScanningFragment());
         ft.addToBackStack(null);
         ft.commit();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            boolean permissionGranted = ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+
+            if (!permissionGranted) {
+                Toast.makeText(this, "You need camera permissions to use the app.", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/paperscanner/camera/PaperScanningFragment.java
+++ app/src/main/java/com/example/paperscanner/camera/PaperScanningFragment.java
@@ -1,20 +1,103 @@
 package com.example.paperscanner.camera;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.paperscanner.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class PaperScanningFragment extends Fragment {
+
+    private static final String TAG = "PaperScanningFragment";
+    private FloatingActionButton captureImageButton;
+    private PreviewView viewfinder;
+    private ImageCapture imageCapture;
+    private ExecutorService imageCaptureExecutor;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_paper_scanning, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        captureImageButton = view.findViewById(R.id.capture_image_button);
+        captureImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                captureImage();
+            }
+        });
+
+        viewfinder = view.findViewById(R.id.viewfinder);
+        viewfinder.setImplementationMode(PreviewView.ImplementationMode.PERFORMANCE);
+
+        startCamera();
+
+        imageCaptureExecutor = Executors.newSingleThreadExecutor();
+    }
+
+    private void startCamera() {
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(getContext());
+
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+
+                Preview preview = new Preview.Builder().build();
+                preview.setSurfaceProvider(viewfinder.getSurfaceProvider());
+
+                imageCapture = new ImageCapture.Builder().build();
+
+                cameraProvider.unbindAll();
+                cameraProvider.bindToLifecycle(getViewLifecycleOwner(), CameraSelector.DEFAULT_BACK_CAMERA, preview);
+
+            } catch (Exception e) {
+                Log.e(TAG, e.toString());
+            }
+        }, ContextCompat.getMainExecutor(getContext()));
+    }
+
+    private void captureImage() {
+        if (imageCapture == null) {
+            return;
+        }
+
+        imageCapture.takePicture(imageCaptureExecutor, new ImageCapture.OnImageCapturedCallback() {
+            @Override
+            public void onCaptureSuccess(@NonNull ImageProxy image) {
+                super.onCaptureSuccess(image);
+
+                // TODO: Process image with opencv, then preview it in another fragment to potentially save it
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        imageCaptureExecutor.shutdown();
     }
 }
